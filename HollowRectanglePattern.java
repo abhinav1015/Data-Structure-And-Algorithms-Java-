2085485f1a0c5c0aed2d87f55e00b1625870c04d
abhinav1015 <maharjanabhinav0@gmail.com>
2021-06-12 08:27:11 +0545

    Finished First Program

--- HollowRectanglePattern.java
+++ HollowRectanglePattern.java
@@ -1,5 +1,16 @@
 public class HollowRectanglePattern {
+    static int row = 5;
+    static int column = 4;
     public static void main(String[] args){
-        System.out.println("Hello");
+        for(int i = 1; i <= row; i++){
+            for(int j = 1; j <= column; j++){
+                if(i == 1 || i == row || j == 1 || j == column){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
     }
+}
 }
